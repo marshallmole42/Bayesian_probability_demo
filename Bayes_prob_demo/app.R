7bbd9a42e8d23a55171e38ad0089a1d15c33fec1
marshall <zhiyuanwang42@gmail.com>
2021-06-13 20:25:52 -0700

    Add plotting for prior & likelihood functions

--- Bayes_prob_demo/app.R
+++ Bayes_prob_demo/app.R
@@ -8,11 +8,15 @@
 #
 
 library(shiny)
-
+library(tidyverse)
 
 likelihood_fun <- function(p, nhit, ntrial) {
     return(p^nhit*(1-p)^(ntrial-nhit)*choose(ntrial, nhit))
 }
+
+
+seq_length = 1001
+xrange = seq(from=0, to=1, length.out=seq_length)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -26,7 +30,7 @@ ui <- fluidPage(
             selectInput(
                 "priorType",
                 "Choose a prior: ",
-                c("Uniform"="u", "Cauchy"="c", "Current posterior (update)"="cp")
+                c("Uniform"="u", "Parabolic"="p", "Current posterior (update)"="cp")
             ), 
             numericInput(
                 "ntrial", 
@@ -44,6 +48,8 @@ ui <- fluidPage(
         # Show a plot of the generated distribution
         mainPanel(
            plotOutput("priorPlot"),
+           #tableOutput("priordata"),
+           plotOutput("likelihoodPlot"),
            plotOutput("posteriorPlot")
         )
     )
@@ -51,9 +57,21 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    
+    #priordata = reactive({tibble(xx=seq(from=0, to=1, length.out=seq_length), prior=ifelse(input$priorType=="u", yes=seq(from=0, to=1, length.out=seq_length), no=seq(from=0, to=1, length.out=seq_length)))})
+    
+    #output$priordata <- renderDataTable({priordata()})
+    
     output$priorPlot <- renderPlot({
         # make the prior plot
+        priordata = tibble(xx=xrange, p=ifelse(c(rep(input$priorType=="u", seq_length)), yes=rep(1, length.out=seq_length), no=(1-4*(xrange-0.5)^2)))
+        priordata = priordata %>% mutate(prior=p/sum(p)*seq_length)
+        ggplot(data=priordata) + geom_line(aes(x=xx, y=prior)) + scale_y_continuous(name="probability density", limits=c(0,2)) + ggtitle("Prior")
+    })
+    
+    output$likelihoodPlot <- renderPlot({
+        likelidata = tibble(xx=xrange, l=likelihood_fun(xrange, input$nhit, input$ntrial))
+        ggplot(data=likelidata) + geom_line(aes(x=xx, y=l)) + ggtitle("Likelihood")
     })
     
     output$posteriorPlot <- renderPlot({
